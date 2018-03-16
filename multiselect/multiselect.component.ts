b1a64cf51d5b09829c5933a788e6aac8e0a93de0
David <davidparro@gmail.com>
2018-03-16 13:44:37 +0100

    update

--- multiselect/multiselect.component.ts
+++ multiselect/multiselect.component.ts
@@ -7,12 +7,15 @@ import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter }
 })
 export class MultiselectComponent implements OnInit  {
     @Input('values') values;
-    @Input('customLabel') customLabel;
-    @Input('height') height;
-    @Input('backgroundSelected') backgroundSelected;
-    @Input('backgroundRemoved') backgroundRemoved;
-    @Input('colorSelected') colorSelected;
-    @Input('colorRemoved') colorRemoved;
+    @Input('customLabel') customLabel = 'Select values';
+    @Input('customMaxLabel') customMaxLabel = 'items selected';
+    @Input('width') width;
+    @Input('height') height = '200px';
+    @Input('backgroundSelected') backgroundSelected = 'rgb(183, 255, 115)';
+    @Input('backgroundRemoved') backgroundRemoved = 'white';
+    @Input('colorSelected') colorSelected = 'inherit';
+    @Input('colorRemoved') colorRemoved = 'inherit';
+    @Input('maxLabelItems') maxLabelItems = 1;
     @ViewChild('listGroup') listGroup: ElementRef;
     @ViewChild('inputSelect') inputSelect: ElementRef;
     @ViewChild('selectBackdrop') selectBackdrop: ElementRef;
@@ -23,23 +26,17 @@ export class MultiselectComponent implements OnInit  {
     }
 
     ngOnInit() {
-        console.log(this.values);
         this.valuesSelected.length = 0;
-        if (this.customLabel) {
-            this.label = this.customLabel;
+        this.label = this.customLabel;
+        if (this.width) {
+            this.listGroup.nativeElement.style.width = this.width;
         } else {
-            this.label = 'Año rehabilitación';
+            this.listGroup.nativeElement.style.width = this.inputSelect.nativeElement.offsetWidth + 'px';
         }
-        this.listGroup.nativeElement.style.width = this.inputSelect.nativeElement.offsetWidth + 'px';
-        if (this.height) {
-            this.listGroup.nativeElement.style.height = this.height;
-        } else {
-            this.listGroup.nativeElement.style.height = '200px';
-        }
+        this.listGroup.nativeElement.style.height = this.height;
     }
 
     toggleShow() {
-        console.log('clicked');
         this.listGroup.nativeElement.classList.toggle('hide');
         this.selectBackdrop.nativeElement.classList.toggle('hide');
     }
@@ -48,28 +45,28 @@ export class MultiselectComponent implements OnInit  {
         if (this.valuesSelected.find(valueSelected => value === valueSelected)) {
             const index = this.valuesSelected.indexOf(value);
             this.valuesSelected.splice(index, 1);
-            if (this.backgroundRemoved) {
-                document.getElementById(value).style.background = this.backgroundRemoved;
-            } else {
-                document.getElementById(value).style.background = 'transparent';
-            }
-            if (this.colorRemoved) {
-                document.getElementById(value).style.color = this.colorRemoved;
-            } else {
-                document.getElementById(value).style.color = 'inherit';
-            }
+            document.getElementById(value).style.background = this.backgroundRemoved;
+            document.getElementById(value).style.color = this.colorRemoved;
         } else {
-            if (this.backgroundSelected) {
-                document.getElementById(value).style.background = this.backgroundSelected;
-            } else {
-                document.getElementById(value).style.background = 'rgb(183, 255, 115)';
-            }
-            if (this.colorSelected) {
-                document.getElementById(value).style.color = this.colorSelected;
-            } else {
-                document.getElementById(value).style.color = 'inherit';
-            }
+            document.getElementById(value).style.background = this.backgroundSelected;
+            document.getElementById(value).style.color = this.colorSelected;
             this.valuesSelected.push(value);
+        }
+        if (this.valuesSelected.length > 0) {
+            this.label = '';
+            this.valuesSelected.forEach((element, index) => {
+                if (this.valuesSelected.length <= this.maxLabelItems) {
+                    if (index === 0) {
+                        this.label += element;
+                    } else {
+                        this.label += ', ' + element;
+                    }
+                } else {
+                    this.label = (index + 1) + ' ' + this.customMaxLabel;
+                }
+            });
+        } else {
+            this.label = this.customLabel;
         }
         this.sendValues.emit(this.valuesSelected);
     }
